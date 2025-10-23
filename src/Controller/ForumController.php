ba5eba3e0e48f067df755fbc55b89feac1241bac
lamia 8512 <elf.lamia@hotmail.fr>
2025-10-23 10:06:03 +0200

    maj

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ForumController extends AbstractController
+{
+    #[Route('/', name: 'app_forum')]
+    public function index(): Response
+    {
+        return $this->render('forum/index.html.twig', [
+            'controller_name' => 'ForumController',
+        ]);
+    }
+
+     #[Route('/apropos', name: 'app_about')]
+    public function apropos(): Response
+    {
+        return $this->render('forum/about.html.twig', []);
+    }
+}
